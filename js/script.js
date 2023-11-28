eb4e867d83f6b36d75761090aca0261e18375527
itsedenhong <itsedenhong@gmail.com>
2023-11-28 14:48:50 -0500

    c
    c

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-const hotspot = document.getElementById('hotspot1');
-hotspot.addEventListener('click', () => {
-  alert('You clicked the hotspot!');
-});
+// script.js 파일 내용
+document.addEventListener("DOMContentLoaded", function() {
+  const hotspot = document.getElementById('hotspot1');
+  if (hotspot) {
+      hotspot.addEventListener('click', () => {
+          alert('You clicked the hotspot!');
+      });
+  } else {
+      console.error("Hotspot element not found!");
+  }
 
-const chairViewer = document.querySelector('model-viewer[src="../models/chair.glb"]');
-const sofaViewer = document.querySelector('model-viewer[src="../models/sofa.glb"]');
-const lampViewer = document.querySelector('model-viewer[src="../models/lamp.glb"]');
+  const chairViewer = document.querySelector('model-viewer[src="./models/chair.glb"]');
+  const sofaViewer = document.querySelector('model-viewer[src="./models/sofa.glb"]');
+  const lampViewer = document.querySelector('model-viewer[src="./models/lamp.glb"]');
 
-chairViewer.addEventListener('click', () => {
-  alert('You clicked the Chair model!');
-});
+  if (chairViewer) {
+      chairViewer.addEventListener('click', () => {
+          alert('You clicked the Chair model!');
+      });
+  } else {
+      console.error("Chair viewer element not found!");
+  }
 
-sofaViewer.addEventListener('click', () => {
-  alert('You clicked the Sofa model!');
-});
+  if (sofaViewer) {
+      sofaViewer.addEventListener('click', () => {
+          alert('You clicked the Sofa model!');
+      });
+  } else {
+      console.error("Sofa viewer element not found!");
+  }
 
-lampViewer.addEventListener('click', () => {
-  alert('You clicked the Lamp model!');
+  if (lampViewer) {
+      lampViewer.addEventListener('click', () => {
+          alert('You clicked the Lamp model!');
+      });
+  } else {
+      console.error("Lamp viewer element not found!");
+  }
 });
