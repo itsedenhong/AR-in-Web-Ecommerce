c5a084ce08bbc1502bd07f0d1ab27da7b35fdcd5
itsedenhong <itsedenhong@gmail.com>
2023-11-28 17:35:28 -0500

    d

--- js/script.js
+++ js/script.js
@@ -1,38 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const hotspot = document.getElementById('hotspot1');
-  if (hotspot) {
-      hotspot.addEventListener('click', () => {
-          alert('You clicked the hotspot!');
-      });
-  } else {
-      console.error("Hotspot element not found!");
-  }
-
-  const chairViewer = document.querySelector('model-viewer[src="./models/chair.glb"]');
-  const sofaViewer = document.querySelector('model-viewer[src="./models/sofa.glb"]');
-  const lampViewer = document.querySelector('model-viewer[src="./models/lamp.glb"]');  
-
-  if (chairViewer) {
-      chairViewer.addEventListener('click', () => {
-          alert('You clicked the Chair model!');
-      });
-  } else {
-      console.error("Chair viewer element not found!");
-  }
-
-  if (sofaViewer) {
-      sofaViewer.addEventListener('click', () => {
-          alert('You clicked the Sofa model!');
-      });
-  } else {
-      console.error("Sofa viewer element not found!");
-  }
-
-  if (lampViewer) {
-      lampViewer.addEventListener('click', () => {
-          alert('You clicked the Lamp model!');
-      });
-  } else {
-      console.error("Lamp viewer element not found!");
-  }
-});
+    const chairViewer = document.getElementById('chair_model');
+    const sofaViewer = document.getElementById('sofa_model');
+    const lampButton = document.getElementById('lamp');
+  
+    chairViewer.addEventListener('click', () => {
+      alert('You clicked the Chair model!');
+    });
+  
+    sofaViewer.addEventListener('click', () => {
+      alert('You clicked the Sofa model!');
+    });
+  
+    lampButton.addEventListener('click', () => {
+      alert('You clicked the Lamp model!');
+    });
+  });
+  
